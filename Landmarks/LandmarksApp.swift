633991a437d071d69170ef9a6d120760f0adcf65
Maciej Hełmecki <mahe03@betssongroup.com>
2024-12-16 13:51:08 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Maciej Helmecki on 16/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
